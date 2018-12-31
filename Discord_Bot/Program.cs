e3ec1e8b2d00ee7e1d04215dbb5a280bab6b390e
FireFlyRT <starwars.98@hotmail.de>
2018-12-31 16:03:17 +0100

    First Commit

---
+++ Discord_Bot/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using System.IO;
+using System.Threading.Tasks;
+using NuGet;
+using Discord;
+
+namespace Discord_Bot {
+	class Program {
+		static void Main(string[] args) {
+			
+		}
+		
+		private DiscordSocketClient _client;
+		public async Task Start() {
+			
+		}
+	}
+}
